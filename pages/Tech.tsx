13c72a24900c05e48684bc4f200366d49fc8c55e
rifxtt <dc.wang840533@gmail.com>
2022-07-15 17:30:57 -0400

    category querty

--- pages/Tech.tsx
+++ pages/Tech.tsx
@@ -5,24 +5,25 @@ import Article from '../components/Article'
 import styles from '../styles/GamesTech.module.scss'
 import {GetServerSideProps } from 'next'
 import {useState, useRef, useCallback} from 'react'
-import {fetchArticles, News, article} from './api/API'
+import {fetchArticles, News, article, categoryQuery} from './api/API'
 
 interface Props{
   data: News;
+  query?: string;
+  list: string[];
 }
 
-const Tech:React.FC<Props> = ({data}) => {
+const Tech:React.FC<Props> = ({data, query, list}) => {
 
-  const [articles, setArticles] = useState<any>(data.articles);
-  const [results, setResults] = useState<number>(data.totalResults);
-  const [query, setQuery] = useState<string>("")
+  const [articles, setArticles] = useState<any>(data.articles)
+  const [results, setResults] = useState<number>(data.totalResults)
   const [loading, setLoading] = useState<boolean>(false)
-  const [pageNum, setPageNum] = useState<number>(1);
+  const [pageNum, setPageNum] = useState<number>(1)
+  const [categories, setCatgories] = useState<string[]>(list)
 
   const observer = useRef<any>()
   
   const lastArticleRef = useCallback((node:HTMLElement) => {
-    if (loading) return 
     if (observer.current) observer.current?.disconnect()
     observer.current = new IntersectionObserver(async entries=> {
       if (entries[0].isIntersecting){
@@ -33,14 +34,28 @@ const Tech:React.FC<Props> = ({data}) => {
   }, [loading])
 
   const getArticles = async ()=> {
+    // let page = pageNum + 1;
+    // setPageNum(page);
+    // console.log(pageNum); 
     setPageNum(currPage => currPage + 1);
-    const newData= await fetchArticles(`${process.env.API_KEY}`, "pc%20AND%20components%20%20AND%20technology", 10, pageNum);
+    console.log(pageNum);
+    // setLoading(true);
+    // const newData= await fetchArticles(`${process.env.API_KEY}`, `${query}`, 10, pageNum);
+    // setLoading(false);
+    // console.log(pageNum);
     // const newArticles = [...articles, ...newData.articles];
     // console.log(newArticles);
 
-    setArticles((prevArticles:article[])=> {
-      return [...prevArticles, ...newData.articles]
-    })
+    // setArticles((prevArticles:article[])=> {
+    //   return [...prevArticles, ...newData.articles]
+    // })
+  }
+
+  const callSomeFunction= () => {
+    setPageNum((currPage:number)=> {
+      return currPage+ 1;
+  });
+    console.log(pageNum);
   }
 
   return (
@@ -49,9 +64,9 @@ const Tech:React.FC<Props> = ({data}) => {
       <div className={styles["gamestech"]}>
         {/* buttons */}
         <div className={styles["button-group"]}>
-          <button>Newest</button>
-          <button>Most relevant</button>
-          <button>Popular</button>
+          {categories.map(category=> (
+            <button onClick = {callSomeFunction}>{category}</button>
+          ))}
         </div>
         {/* articles */}
         <div className={styles["article-group"]}>
@@ -76,18 +91,22 @@ const Tech:React.FC<Props> = ({data}) => {
       </div> {/* .games div*/}
     </>
   );
-
   
 }
+
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
 
   const dotenv = require('dotenv').config()
 
-  const data = await fetchArticles(`${process.env.API_KEY}`, "pc%20AND%20components%20%20AND%20technology", 10, 1);
+  const query = "pc%20AND%20components%20%AND%20technology";
+
+  const list = ["Tech", "PC Parts", "Nvidia", "FAANG"];
+
+  const data = await fetchArticles(`${process.env.API_KEY}`, query, 10, 1);
 
   return {
-    props: {data}
+    props: {data, query, list}
   }
 
 }
