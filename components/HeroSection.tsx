710b587a9379450975589610682747eb3ecde152
rifxtt <dc.wang547@gmail.com>
2022-07-19 15:41:27 -0400

    footer layout

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,18 +1,32 @@
 import styles from '../styles/Hero.module.scss'
 import {IoMdArrowDropdown} from 'react-icons/io'
-import Navbar from '../components/Navbar'
 
 const HeroSection:React.FC = () => {
   return (
-      <div className= {styles.heroContainer}>
-        <video src= '/videos/Game_room.mp4' autoPlay loop muted></video>
-        <div className={styles.mainDesc}>
-            <h1>games and tech news</h1>
-            <h3>start browsing today</h3>
+      <>
+        <section className= {styles.heroContainer}>
+          <video src= '/videos/Game_room.mp4' autoPlay loop muted></video>
+          <div className={styles.mainDesc}>
+              <h1>games and tech news</h1>
+              <h3>start browsing today</h3>
+          </div>
+          <div className= {styles.dropdownIcon}><IoMdArrowDropdown/></div>
+          {/* insert the scroll icon here */}
+        </section>
+        <section className= {styles.videoContainer}>
+          <div className= {styles.videoCarousel}>
+            <h1>Video here</h1>
+          </div>
+          <div className= {styles.dropdownIcon}><IoMdArrowDropdown/></div>
+        </section>
+        <section className= {styles.storyContainer}>
+          <h1>Today's Top Stories</h1>
+        <div className= {styles.storyCarousel}>
+          <h3>stories carousel here</h3>
         </div>
         <div className= {styles.dropdownIcon}><IoMdArrowDropdown/></div>
-        {/* insert the scroll icon here */}
-      </div>
+        </section>
+      </>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import {NextPage} from 'next'
+import Footer from '../components/Footer'
 import Hero from '../components/HeroSection'
 import Navbar from '../components/Navbar'
+import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className= {styles.homeContainer}>
     <Navbar/>
     <Hero/>
-    </>
+    <Footer/>
+    </div>
   )
 }
 
