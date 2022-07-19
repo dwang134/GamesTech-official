fb64125aef24dc86d1d69acd0df02eeac346108c
rifxtt <dc.wang547@gmail.com>
2022-07-19 15:34:16 -0400

    video carousel

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEWS_API_KEY: '76050172983746038160371647c77be9',
-    YT_API_KEY: 'AIzaSyDiR5hfpbmNQStehjIQmRYHaRbwaTsn3sQ'
+    YT_API_KEY: 'AIzaSyDiR5hfpbmNQStehjIQmRYHaRbwaTsn3sQ',
+    CHANNEL_ID: 'UCbu2SsF-Or3Rsn3NxqODImw',
   }
 }
 
