c5d22cef673d23afbad3788cfaa6e816c634fa38
rifxtt <dc.wang547@gmail.com>
2022-07-19 15:41:28 -0400

    page not adding

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    API_KEY: '76050172983746038160371647c77be9'
+  }
 }
 
 module.exports = nextConfig
