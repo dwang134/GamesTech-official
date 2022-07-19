2b5bfbb3ce979d1fd0085729bd0ac940970ee464
rifxtt <dc.wang547@gmail.com>
2022-07-19 15:34:13 -0400

    gamestech

--- pages/Tech.tsx
+++ pages/Tech.tsx
@@ -1,8 +1,71 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import styles from '../styles/GamesTech.module.scss'
 
 const Tech:React.FC = () => {
   return (
-    <div>Tech</div>
+    <>
+      <Navbar />
+      <div className={styles["games"]}> 
+      {/* buttons */}
+        <div className= {styles["button-group"]}>
+          <button>Newest</button>
+          <button>Most relevant</button>
+          <button>Popular</button>
+        </div>
+        {/* articles */}
+        <div className= {styles["article-group"]}>
+          <div className={styles["article"]}>
+            <div className={styles["article__img"]}>Image placeholder</div>
+            <div className={styles["article__info"]}>
+              <h2 className={styles["article__title"]}>
+                Article title goes here
+              </h2>
+              <h4 className={styles["article__desc"]}>
+                Article description goes here
+              </h4>
+              <p className={styles["article__source"]}>Source</p>
+            </div>
+          </div>
+          <div className={styles["article"]}>
+            <div className={styles["article__img"]}>Image placeholder</div>
+            <div className={styles["article__info"]}>
+              <h2 className={styles["article__title"]}>
+                Article title goes here
+              </h2>
+              <h4 className={styles["article__desc"]}>
+                Article description goes here
+              </h4>
+              <p className={styles["article__source"]}>Source</p>
+            </div>
+          </div>
+          <div className={styles["article"]}>
+            <div className={styles["article__img"]}>Image placeholder</div>
+            <div className={styles["article__info"]}>
+              <h2 className={styles["article__title"]}>
+                Article title goes here
+              </h2>
+              <h4 className={styles["article__desc"]}>
+                Article description goes here
+              </h4>
+              <p className={styles["article__source"]}>Source</p>
+            </div>
+          </div>
+          <div className={styles["article"]}>
+            <div className={styles["article__img"]}>Image placeholder</div>
+            <div className={styles["article__info"]}>
+              <h2 className={styles["article__title"]}>
+                Article title goes here
+              </h2>
+              <h4 className={styles["article__desc"]}>
+                Article description goes here
+              </h4>
+              <p className={styles["article__source"]}>Source</p>
+            </div>
+          </div>
+        </div>  {/* .article-group */}
+      </div> {/* .games div*/}
+    </>
   )
 }
 
--- pages/games.tsx
+++ pages/games.tsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import Navbar from '../components/Navbar'
+import styles from '../styles/GamesTech.module.scss'
+import Image from 'next/image'
 
-const games = () => {
+const Games:React.FC = () => {
   return (
-    <div>games</div>
-  )
+    <>
+      <Navbar />
+      <div className={styles["games"]}> 
+      {/* buttons */}
+        <div className= {styles["button-group"]}>
+          <button>Newest</button>
+          <button>Most relevant</button>
+          <button>Popular</button>
+        </div>
+        {/* articles */}
+        <div className= {styles["article-group"]}>
+          <div className={styles["article"]}>
+            <div className={styles["article__img"]}>Image placeholder</div>
+            <div className={styles["article__info"]}>
+              <h2 className={styles["article__title"]}>
+                Article title goes here
+              </h2>
+              <h4 className={styles["article__desc"]}>
+                Article description goes here
+              </h4>
+              <p className={styles["article__source"]}>Source</p>
+            </div>
+          </div>
+          <div className={styles["article"]}>
+            <div className={styles["article__img"]}>Image placeholder</div>
+            <div className={styles["article__info"]}>
+              <h2 className={styles["article__title"]}>
+                Article title goes here
+              </h2>
+              <h4 className={styles["article__desc"]}>
+                Article description goes here
+              </h4>
+              <p className={styles["article__source"]}>Source</p>
+            </div>
+          </div>
+          <div className={styles["article"]}>
+            <div className={styles["article__img"]}>Image placeholder</div>
+            <div className={styles["article__info"]}>
+              <h2 className={styles["article__title"]}>
+                Article title goes here
+              </h2>
+              <h4 className={styles["article__desc"]}>
+                Article description goes here
+              </h4>
+              <p className={styles["article__source"]}>Source</p>
+            </div>
+          </div>
+          <div className={styles["article"]}>
+            <div className={styles["article__img"]}>Image placeholder</div>
+            <div className={styles["article__info"]}>
+              <h2 className={styles["article__title"]}>
+                Article title goes here
+              </h2>
+              <h4 className={styles["article__desc"]}>
+                Article description goes here
+              </h4>
+              <p className={styles["article__source"]}>Source</p>
+            </div>
+          </div>
+        </div>  {/* .article-group */}
+      </div> {/* .games div*/}
+    </>
+  );
 }
 
-export default games
+export default Games
